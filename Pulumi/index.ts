d1752fe130eab733fa3246814d5f65302d087814
Nolan Heimlich <nolan.heimlich@gmail.com>
2020-12-29 18:16:10 -0500

    added VPC & Subnets

--- Pulumi/index.ts
+++ Pulumi/index.ts
@@ -2,9 +2,35 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
+//VPC & Subnets
+const vpcNetwork = new gcp.compute.Network("vpc_network", { autoCreateSubnetworks: false, name:"nhlabs",});
+const Network_Range1 = new gcp.compute.Subnetwork("range1", {
+    ipCidrRange: "172.1.240.0/24",
+    region: "us-east1",
+    network: vpcNetwork.id,
+});
+const Network_Range2 = new gcp.compute.Subnetwork("range2", {
+    ipCidrRange: "172.1.241.0/24",
+    region: "us-east1",
+    network: vpcNetwork.id,
+});
+const Network_Range3 = new gcp.compute.Subnetwork("range3", {
+    ipCidrRange: "172.1.242.0/24",
+    region: "us-east1",
+    network: vpcNetwork.id,
+});
+
+//Instances
+/*
+const Debian_IP = new gcp.compute.Address("static", {
+    address: "172.1.240.5",
+    region: "us-east",
+});
+
+
 // Create a sample Ubuntu Instance in Pulumi
 const instance1 = new gcp.compute.Instance("My-Instance", {
-    hostname: "Ubuntu.nhlabs.org",
+    name: "debian_MYSQL",
     bootDisk: {
         initializeParams: {
             image: 	"ubuntu-2004-lts",
@@ -13,12 +39,11 @@ const instance1 = new gcp.compute.Instance("My-Instance", {
     machineType: "f1-micro",
     zone: "us-east1-b",
     networkInterfaces: [{
-        accessConfigs: [{}],
+        network: "default",
     }],
-    name: "nubuntu"
+    
     
 
 });
-// Export the DNS name of the bucket
-export const url = instance1.hostname;
-export const ip = instance1.ipAddress;
+//Exports
+*/
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # CI-Project-CCDC
 
 A project using Puppet and Pulumi to Deploy Competition Infrastructure. 
+
+
+172.1.240.x --
+1 - CentOS
+1 - Debian
+
+172.1.241.x --
+1 - Ubuntu
+1 - 2008 AD
+1 - Win 8.1
+
+172.1.242.x --
+2 - CentOS
+1 - Fedora
