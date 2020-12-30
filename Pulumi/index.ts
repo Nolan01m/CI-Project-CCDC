135a9327870858b179dafcb5d365fa61304434c6
Nolan Heimlich <nolan.heimlich@gmail.com>
2020-12-29 21:59:18 -0500

    added another instance

--- Pulumi/index.ts
+++ Pulumi/index.ts
@@ -3,8 +3,8 @@ import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
 //VPC & Subnets
-const vpcNetwork = new gcp.compute.Network("vpc_network", 
-    {autoCreateSubnetworks: false, name:"nhlabs", routingMode:"REGIONAL",});
+const vpcNetwork = new gcp.compute.Network("vpc_network",
+    { autoCreateSubnetworks: false, name: "nhlabs", routingMode: "REGIONAL", });
 
 const Network_Range1 = new gcp.compute.Subnetwork("range1", {
     ipCidrRange: "172.1.240.0/24",
@@ -23,34 +23,51 @@ const Network_Range3 = new gcp.compute.Subnetwork("range3", {
 });
 
 //Adresses
-
 const Debian_IP = new gcp.compute.Address("debian", {
     addressType: 'INTERNAL',
     address: "172.1.240.5",
     region: "us-east1",
-    subnetwork:Network_Range1.id,
+    subnetwork: Network_Range1.id,
+});
+const Phantom_IP = new gcp.compute.Address("phantom", {
+    addressType: 'INTERNAL',
+    address: "172.1.240.6",
+    region: "us-east1",
+    subnetwork: Network_Range1.id,
 });
 
-
 // Create a sample Ubuntu Instance in Pulumi
-const instance1 = new gcp.compute.Instance("My-Instance", {
+const computeInstance1 = new gcp.compute.Instance("Debian", {
     name: "debiansql",
     bootDisk: {
         initializeParams: {
-            image: 	"debian-10",
+            image: "debian-10",
         },
     },
     machineType: "f1-micro",
     zone: "us-east1-b",
     networkInterfaces: [{
-        subnetwork:Network_Range1.id,
-        networkIp:Debian_IP.id,
-        accessConfigs:[{}],
+        subnetwork: Network_Range1.id,
+        networkIp: Debian_IP.id,
+        accessConfigs: [{}],
     }],
-    
-    
-    
-
 });
+
+const computeInstance2 = new gcp.compute.Instance("Phantom", {
+    name: "centosphantom",
+    bootDisk: {
+        initializeParams: {
+            image: "centos-8",
+        },
+    },
+    machineType: "f1-micro",
+    zone: "us-east1-b",
+    networkInterfaces: [{
+        subnetwork: Network_Range1.id,
+        networkIp: Phantom_IP.id,
+        accessConfigs: [{}],
+    }],
+});
+
 //Outputs
 //export const InstanceIP = instance1.networkInterfaces[0].accessConfigs[0].natIp;
