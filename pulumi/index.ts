28b5c55eb3052d4f91f01d0afc64567606b622ac
Nolan Heimlich <nolan.heimlich@gmail.com>
2021-03-31 21:48:37 -0400

    adding base network to topology

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,20 +1,39 @@
 import * as pulumi from "@pulumi/pulumi";
+
 import * as resources from "@pulumi/azure-native/resources";
-import * as storage from "@pulumi/azure-native/storage";
+//import * as storage from "@pulumi/azure-native/storage";
+import * as network from "@pulumi/azure-native/network";
 
-// Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup");
+//const config = new pulumi.Config();
+//const username = config.require("jcccdc");
+//const password = config.requireSecret("P@ssw0rd");
 
-// Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sa", {
-    resourceGroupName: resourceGroup.name,
-    sku: {
-        name: storage.SkuName.Standard_LRS,
+// Azure Resource Group -- Similar to a VPC
+const ccdc = new resources.ResourceGroup("jc-ccdc").name;
+
+// Create a network and subnet for all VMs.
+const Virt_Network = new network.VirtualNetwork("topology-network", {
+    resourceGroupName: ccdc,
+    addressSpace: { addressPrefixes: ["172.20.240.0/23"] },
+    subnets: [{
+        name: "Internal",
+        addressPrefix: "172.20.240.0/25",
     },
-    kind: storage.Kind.StorageV2,
+    {
+        name: "Public",
+        addressPrefix: "172.20.241.0/25",
+    },
+    {
+        name: "User",
+        addressPrefix: "172.20.242.0/25",
+    }],
 });
+const publicIp = new network.PublicIPAddress("External Access IP", {
+    resourceGroupName: ccdc,
+    publicIPAllocationMethod: network.IPAllocationMethod.Dynamic,
+},{parent: Virt_Network});
 
-// Export the primary key of the Storage Account
-const storageAccountKeys = pulumi.all([resourceGroup.name, storageAccount.name]).apply(([resourceGroupName, accountName]) =>
-    storage.listStorageAccountKeys({ resourceGroupName, accountName }));
-export const primaryStorageKey = storageAccountKeys.keys[0].value;
+
+// Internal 172.20.240.0
+// Public 172.20.241.0
+// User 172.20.242.0
